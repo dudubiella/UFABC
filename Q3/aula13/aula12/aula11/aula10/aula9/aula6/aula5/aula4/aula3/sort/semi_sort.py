650c88466c31086abd2e0b99a7e91569c0a158a9
Biella <eduardo.biella@gmail.com>
2025-04-04 20:49:17 -0300

    merge sort

--- Q3/aula13/aula12/aula11/aula10/aula9/aula6/aula5/aula4/aula3/sort/semi_sort.py
+++ Q3/aula13/aula12/aula11/aula10/aula9/aula6/aula5/aula4/aula3/sort/semi_sort.py
@@ -1,4 +1,4 @@
-def semi_sort(lista: list, p: int, q: int, r: int = -1) -> list:
+def merge(lista: list, q: int, p: int = 0, r: int = -1) -> list:
     if r == -1: r = len(lista)
     lista_ord = [0 for a in range(len(lista))]
     i, j, k = p, q, 0
@@ -6,7 +6,6 @@ def semi_sort(lista: list, p: int, q: int, r: int = -1) -> list:
         if lista[i] <= lista[j]:
             lista_ord[k] = lista[i]
             i += 1
-
         else:
             lista_ord[k] = lista[j]
             j += 1
@@ -14,10 +13,17 @@ def semi_sort(lista: list, p: int, q: int, r: int = -1) -> list:
     if j < r:
         lista_ord[k:] = lista[j:r]
     else:
-        lista_ord[k:] = lista[i:r]
+        lista_ord[k:] = lista[i:q]
     return lista_ord
 
-def merge_sort
+def merge_sort(lista):
+    if len(lista) < 2: return lista
+    else:
+        meio = len(lista) // 2
+        print(meio)
+        direita = merge_sort(lista[meio:])
+        esquerda = merge_sort(lista[:meio])
+        print(merge(direita + esquerda, len(direita)))
+        return merge(direita + esquerda, len(direita))
 
-print(semi_sort([1,1,3,4,0,2,3,5,8], 0, 4, 10))
-
+print(merge_sort([0,12,4,1,-1,3,5]))
