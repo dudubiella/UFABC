bf308741e3d3ca07fce632d3ed4b8d278010a45f
Biella <eduardo.biella@gmail.com>
2025-04-22 17:06:19 -0300

    digrafos

--- Q3/aula17/aula16/matriz.py
+++ Q3/aula17/aula16/matriz.py
@@ -1,33 +1,42 @@
 class Matriz:
-    def __init__(self, mat):
+    def __init__( self, mat: [[float]]):
         self.mat = mat
-        self.lin = len(mat)
-        self.col = len(mat[0])
+        self.lin = len (mat)
+        self.col = len (mat[0])
 
-    def Linha(self, n):
+    def Linha (self, n: int):
         return [i for i in self.mat[n]]
 
-    def Coluna(self, n):
+    def Coluna (self, n: int):
         return [i[n] for i in self.mat]
 
-    def __mul__(self, mat2: [[int]]):
+    def __mul__ (self, mat2: [[float]]) -> [[float]]:
         matResultado = []
-        for i in range(self.lin):           
-            matResultado.append([])
-            for j in range(mat2.col):
-                listMult = [x*y for x, y in zip(self.Linha(i), mat2.Coluna(j))]
-                matResultado[i].append(sum(listMult))
-        return matResultado
+        for i in range (self.lin):           
+            matResultado.append ([])
+            for j in range (mat2.col):
+                listMult = [x * y for x, y in zip (self.Linha (i), mat2.Coluna (j))]
+                matResultado[i].append (sum (listMult))
+        resultado = Matriz (matResultado)
+        return resultado
+    
+    def __add__ (self, mat2: [[float]]) -> [[float]]:
+        if self.lin != mat2.lin or self.col != mat2.col: return [[]]
+        matResultado = [[x + y for x, y in zip (self.Linha (i), mat2.Linha (i))]for i in range(self.lin)]
+        resultado = Matriz (matResultado)
+        return resultado
 
-    def imprime(self) -> None:
-        [print(self.Linha(i)) for i in range(self.lin)]
-        return
 
-def main():
-    mat1 = Matriz([[2, 3], [4, 6]])
-    mat2 = Matriz([[1, 3, 0], [2, 1, 1]])
-    mat3 = Matriz(mat1*mat2)
-    mat3.imprime()
+def imprime_mat (M: [[float]]) -> None:
+    try: [print (M.Linha(i)) for i in range (M.lin)]
+    except: [print (M[i]) for i in range (len (M))]
+    return
+
+def main () -> None:
+    mat1 = Matriz ([[2, 3, 2], [4, 6, 0]])
+    mat2 = Matriz ([[1, 3, 0], [2, 1, 1]])
+    mat3 = mat1*mat2
+    imprime_mat (mat3)
 
 if __name__ == "__main__":
-    main()
+    main ()
--- Q3/aula17/digrafo.py
+++ Q3/aula17/digrafo.py
@@ -1,14 +1,56 @@
 from math import inf as oo
-from aula16.matriz import Matriz
+from aula16.matriz import Matriz, imprime_mat
 
 class Digrafo:
-    def __init__(self, m, w):
-        self.M = Matriz(m)
-        self.W = Matriz(w)
+    def __init__ (self, m, w):
+        self.M = Matriz (m)
+        self.W = Matriz (w)
+    
+    def existe_caminho_tam_n (self, a: int, b: int, n: int):
+        chega = self.M
+        for _ in range (n - 1):
+            chega *= self.M
+            if chega.mat[a][b] == 1:
+                return True
+        return False
 
-def main():
-    D = Digrafo([[0,1], [0,0]],[[oo, 1],[oo,oo]])
-    D.M.imprime()
+
+    def USSSP (self, s: int):
+        d = [oo] * (n := self.M.lin); d[s] = 0
+        p = [j for j in range (n)]
+        Q = [s]
+        while Q != []:
+            i = Q.pop (0)
+            for j in range(n):
+                if self.M.mat[i][j] and d[i] + 1 < d[j]:
+                    d[j] = d[i] + 1
+                    p[j] = i
+                    Q.append (j)
+        self.d, self.p = d, p
+        return
+
+    def BFS (self, s: int) -> [int]:
+        self.USSSP(s)
+        return self.d
+
+    def print_path (self, s: int, t: int, vez = 0) -> None:
+        self.USSSP(s)
+        if self.p[t] == t: 
+            print (t, end = " ")
+        else:
+            self.print_path (s, self.p[t], 1)
+            print ("-->", t, end = " ")
+        if vez == 0: print()
+        return
+    
+    def is_connected (self) -> bool:
+        d = self.BFS (0)
+        return all (di < oo for di in d)
+
+def main() -> None:
+    D = Digrafo([[0, 1, 1], [0, 0, 1], [1, 0, 0]], [[oo, 1, 4], [oo, oo, 2], [3, oo, oo]])
+    D.print_path(2,1)
+    print(D.is_connected())
 
 if __name__ == "__main__":
     main()
