88002bf18e66dd8754b91908fb4de969d1d95661
Biella <eduardo.biella@gmail.com>
2025-04-25 21:04:24 -0300

    aula 22

---
+++ Q3/aula22/permutacao.py
@@ -0,0 +1,13 @@
+def permute(n: int):
+    N = [a for a in range(n)]
+    i = n
+    print(N)
+    while i > 0:
+        return
+
+
+def main() -> None:
+    permute(1)
+
+if __name__ == "__main__":
+    main()
